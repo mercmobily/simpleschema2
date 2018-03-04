fb7671b8703d2714b13092b8558bb34d1180e4ef
merc <merc@mobily1.com>
2018-03-04 23:58:54 +0800

    First rewrite

--- SimpleSchema.js
+++ SimpleSchema.js
@@ -8,26 +8,6 @@ The above copyright notice and this permission notice shall be included in all c
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
-/*
-TODO:
-  X Make sure _cast uses an object much like _param for consistency
-
-  X Make it actually run/work
-
-  - Rename object variables so that they make more sense
-
-  - See if the cycle in param can be the same in cast:
-    if (options.onlyObjectValues) targetObject = object
-    else targetObject = this.structure
-
-  - See if the code can be written without so many side effects
-
-  - Turn validate, _cast and _param into await/async code
-
-  - Check if "required" can be treated like a normal parameter, no special treatment
-
-*/
-
 var CircularJSON = require('circular-json')
 
 var SimpleSchema = class {
@@ -49,8 +29,7 @@ var SimpleSchema = class {
 
     // No toString() available: failing to cast
     if (typeof (p.value.toString) === 'undefined') {
-      p.failedCasts[ p.fieldName ] = true
-      return
+      throw this.castError(p.fieldName)
     }
 
     // Return cast value
@@ -72,8 +51,7 @@ var SimpleSchema = class {
     // If Number() returns NaN, fail
     var r = Number(p.value)
     if (isNaN(r)) {
-      p.failedCasts[ p.fieldName ] = true
-      return p.value
+      throw this.castError(p.fieldName)
     }
 
     // Return cast value
@@ -89,8 +67,7 @@ var SimpleSchema = class {
     // If new Date() returns NaN, date was not corect, fail
     var r = new Date(p.value)
     if (isNaN(r)) {
-      p.failedCasts[ p.fieldName ] = true
-      return p.value
+      throw this.castError(p.fieldName)
     }
 
     // return cast value
@@ -106,8 +83,7 @@ var SimpleSchema = class {
 
     if (p.options.deserialize) {
       if (typeof (p.value) !== 'string') {
-        p.failedCasts[ p.fieldName ] = true
-        return p.value
+        throw this.castError(p.fieldName)
       }
 
       try {
@@ -117,8 +93,7 @@ var SimpleSchema = class {
           // It worked: return r
         return r
       } catch (e) {
-        p.failedCasts[ p.fieldName ] = true
-        return p.value
+        throw this.castError(p.fieldName)
       }
     } else {
       try {
@@ -127,8 +102,7 @@ var SimpleSchema = class {
           // It worked: return r
         return r
       } catch (e) {
-        p.failedCasts[ p.fieldName ] = true
-        return p.value
+        throw this.castError(p.fieldName)
       }
 
     //
@@ -152,8 +126,7 @@ var SimpleSchema = class {
   idType (p) {
     var n = parseInt(p.value)
     if (isNaN(n)) {
-      p.failedCasts[ p.fieldName ] = true
-      return p.value
+      throw this.castError(p.fieldName)
     } else {
       return n
     }
@@ -163,20 +136,20 @@ var SimpleSchema = class {
 
   minParam (p) {
     if (p.definition.type === 'number' && p.value && p.value < p.parameterValue) {
-      p.errors.push({ field: p.fieldName, message: 'Field is too low: ' + p.fieldName })
+      throw this.typeError(p.fieldName, "Field's value is too low")
     }
     if (p.definition.type === 'string' && p.value && p.value.length < p.parameterValue) {
-      p.errors.push({ field: p.fieldName, message: 'Field is too short: ' + p.fieldName })
+      throw this.typeError(p.fieldName, 'Field is too short')
     }
   }
 
   maxParam (p) {
     if (p.definition.type === 'number' && p.value && p.value > p.parameterValue) {
-      p.errors.push({ field: p.fieldName, message: 'Field is too high: ' + p.fieldName })
+      throw this.typeError(p.fieldName, "Field's value is too high")
     }
 
     if (p.definition.type === 'string' && p.value && p.value.length > p.parameterValue) {
-      p.errors.push({ field: p.fieldName, message: 'Field is too long: ' + p.fieldName })
+      throw this.typeError(p.fieldName, 'Field is too low')
     }
   }
 
@@ -186,7 +159,7 @@ var SimpleSchema = class {
     }
 
     var r = p.parameterValue.call(this, p.object, p.object[ p.fieldName ], p.fieldName)
-    if (typeof (r) === 'string') p.errors.push({ field: p.fieldName, message: r })
+    if (typeof (r) === 'string') throw this.typeError(p.fieldName, r)
   }
 
   uppercaseParam (p) {
@@ -215,194 +188,132 @@ var SimpleSchema = class {
     }
   }
 
-  requiredParam (p) {
-    if (typeof (p.objectBeforeCast[ p.fieldName ]) === 'undefined' && p.parameterValue) {
-      p.errors.push({ field: p.fieldName, message: 'Field required: ' + p.fieldName })
-    }
-  }
-
   notEmptyParam (p) {
     var bc = p.objectBeforeCast[ p.fieldName ]
     var bcs = typeof (bc) !== 'undefined' && bc !== null && bc.toString ? bc.toString() : ''
     if (!Array.isArray(p.value) && typeof (bc) !== 'undefined' && bcs === '' && p.parameterValue) {
-      p.errors.push({ field: p.fieldName, message: 'Field cannot be empty: ' + p.fieldName })
+      throw this.typeError(p.fieldName, 'Field cannot be empty')
     }
   }
 
-  // Options and values used:
-  //  * options.onlyObjectValues              -- Will apply cast for existing object's keys rather than the
-  //                                             schema itself
-  //  * options.skipCast                      -- To know what casts need to be skipped
+  castError (field) {
+    var e = new Error('Error with field: ' + field)
+    e.errorObject = { field, message: 'Error during casting' }
+    return e
+  }
+
+  typeError (field, message) {
+    var e = new Error(message)
+    e.errorObject = { field, message }
+    return e
+  }
+
+  // Options:
   //
-  //  * this.structure[ fieldName ].required  -- To skip cast if it's `undefined` and it's NOT required
-  //  //* this.structure[ fieldName ].protected -- To skip cast if it's `undefined` and it's protected
+  //  * options.onlyObjectValues             -- Will apply cast for existing object's keys rather than the schema itself
+  //  * options.skipFields                   -- To know what casts need to be skipped
+  //  * options.skipParams                   -- Won't apply specific params for specific fields
   //
-  _cast (object, options, cb) {
-    var failedCasts = {}
-    var failedRequired = {}
-    options = typeof (options) === 'undefined' ? {} : options
+  // This will run _cast and _param
+  validate (object, options) {
+    var errors = []
+    var skipCast
+    var validatedObject
     var targetObject
-    var castObject = {}
+    var fieldName
+
+    // Copy object over
+    validatedObject = Object.assign({}, object)
+
+    for (fieldName in object) {
+      if (typeof this.structure[ fieldName ] === 'undefined') {
+        errors.push({ field: fieldName, message: 'Field not allowed' })
+      }
+    }
 
     // Set the targetObject. If the target is the object itself,
     // then missing fields won't be a problem
     if (options.onlyObjectValues) targetObject = object
     else targetObject = this.structure
 
-    for (var fieldName in targetObject) {
-      // Getting the definition
+    for (fieldName in targetObject) {
       var definition = this.structure[ fieldName ]
 
-      // Copying the value over
-      if (typeof (object[fieldName]) !== 'undefined') castObject[ fieldName ] = object[ fieldName ]
+      skipCast = false
 
-      // If the definition is undefined, and it's an object-values only check,
-      // then the missing definition mustn't be a problem.
-      if (typeof (definition) === 'undefined' && options.onlyObjectValues) continue
-
-      // Skip casting if so required by the skipCast array
-      if (Array.isArray(options.skipCast) && options.skipCast.indexOf(fieldName) !== -1) {
-        continue
+      // Skip cast/param if so required by the skipField array
+      if (Array.isArray(options.skipFields) && options.skipFields.indexOf(fieldName) !== -1) {
+        skipCast = true
       }
-
       // Skip casting if value is `undefined` AND it's not required
       if (!definition.required && typeof (object[ fieldName ]) === 'undefined') {
-        continue
+        skipCast = true
+      }
+      // Skip casting if value is `undefined` AND it IS required (enriching error)
+      // NOTE: this won't happen if 'required' is in the list of parameters to be skipped
+      if (definition.required && typeof (object[ fieldName ]) === 'undefined') {
+        if (!this._paramToBeSkipped(fieldName, 'required', options.skipParams)) {
+          skipCast = true
+          errors.push({ field: fieldName, message: 'Field required: ' + fieldName })
+        }
       }
 
-      // Skip casting if value is `undefined` AND it IS required
-      // Also, set failedRequired for that field, so that no param will be applied to it except `required`
-      if (definition.required && typeof (object[ fieldName ]) === 'undefined') {
-        failedRequired[ fieldName ] = true
-        continue
-      }
+      // If cast is skipped for whatever reason, params will never go through either
+      if (skipCast) continue
 
       // Run the xxxType function for a specific type
       if (typeof (this[ definition.type + 'Type' ]) === 'function') {
-        var result = this[ definition.type + 'Type' ]({
-          definition,
-          value: object[ fieldName ],
-          fieldName,
-          options,
-          failedCasts
-        })
-
-        if (typeof (result) !== 'undefined') castObject[ fieldName ] = result
+        try {
+          var result = this[ definition.type + 'Type' ]({
+            definition,
+            value: object[ fieldName ],
+            fieldName,
+            object: validatedObject,
+            objectBeforeCast: object,
+            options
+          })
+        } catch (e) {
+          errors.push(e.errorObject)
+        }
+        if (typeof result !== 'undefined') validatedObject[ fieldName ] = result
       } else {
         throw (new Error('No casting function found, type probably wrong: ' + definition.type))
       }
-    }
 
-    // That's it -- return resulting Object
-    return { castObject, failedCasts, failedRequired }
-  }
-
-  // Options and values used:
-  //  * options.onlyObjectValues             -- Will skip appling parameters if undefined and
-  //                                            options.onlyObjectValues is true
-  //  * options.skipParams                   -- Won't apply specific params for specific fields
-
-  _params (object, objectBeforeCast, options, failedCasts, failedRequired, cb) {
-    options = typeof (options) === 'undefined' ? {} : options
-
-    var targetObject
-    var errors = []
-    var paramObject = {}
-
-    for (var k in objectBeforeCast) {
-      if (typeof (this.structure[ k ]) === 'undefined') {
-        errors.push({ field: k, message: 'Field not allowed: ' + k })
-      }
-    }
-
-    // Copying object into paramObject
-    for (k in object) {
-      if (typeof (object[ k ]) !== 'undefined') paramObject[ k ] = object[ k ]
-    }
-
-    // Set the targetObject. If the target is the object itself,
-    // then missing fields won't be a problem
-    if (options.onlyObjectValues) targetObject = object
-    else targetObject = this.structure
-
-    for (var fieldName in targetObject) {
-      // Scan schema
-      var definition = this.structure[ fieldName ]
-
-      // The `onlyObjectValues` option is on: skip anything that is not in the object
-      if (options.onlyObjectValues && typeof (object[ fieldName ]) === 'undefined') continue
-
-      // If cast failed, then don't bother with the parameters
-      if (failedCasts[ fieldName ]) continue
-
-      definition = this.structure[ fieldName ]
-
-      // Run specific functions based on the passed options
-
-      // If `required` failed during casting, then skip other parameters --
-      // `required` is the ONLY parameter that will actually get called
-      var def = failedRequired[fieldName] ? { required: true } : definition
-
-      for (var parameterName in def) {
+      for (var parameterName in this.structure[ fieldName ]) {
+        //
         // If it's to be skipped, we shall skip -- e.g. `options.skipParams == { tabId: 'required' }` to
         // skip `required` parameter for `tabId` field
-        if (typeof (options.skipParams) === 'object' && options.skipParams !== null) {
-          var skipParams = options.skipParams[ fieldName ]
-          if (Array.isArray(skipParams) && skipParams.indexOf(parameterName) !== -1) continue
-        }
+        if (this._paramToBeSkipped(fieldName, parameterName, options.skipParams)) continue
 
         if (parameterName !== 'type' && typeof (this[ parameterName + 'Param' ]) === 'function') {
-          // Store the length of errors; later, it will use this to check that it hasn't grown
-          var errLength = errors.length
+          try {
+            result = this[ parameterName + 'Param' ]({
+              definition,
+              value: validatedObject[ fieldName ],
+              fieldName,
+              object: validatedObject,
+              objectBeforeParams: object,
+              valueBeforeParams: object[ fieldName ],
+              parameterName,
+              parameterValue: definition[ parameterName ],
+              options: options
+            })
+          } catch (e) {
+            errors.push(e.errorObject)
+          }
 
-          var result = this[ parameterName + 'Param' ]({
-            definition: definition,
-            fieldName: fieldName,
-            parameterName: parameterName,
-            parameterValue: definition[ parameterName ],
-            options: options,
-            object: paramObject,
-            objectBeforeCast: objectBeforeCast,
-            objectBeforeParams: object,
-            value: paramObject[ fieldName ],
-            valueBeforeParams: object[ fieldName ],
-            errors: errors
-          })
-
-          if (typeof (result) !== 'undefined') paramObject[ fieldName ] = result
-
-          // If `errors` grew, the following parameters will not be applied
-          if (errors.length !== errLength) break
+          if (typeof (result) !== 'undefined') validatedObject[ fieldName ] = result
         }
       }
     }
-    return { paramObject, errors }
+    return { validatedObject, errors }
   }
 
-  // Options and values used (the ones used by _cast() and _params() together)
-  //
-  //  * options.onlyObjectValues             -- Will apply cast for existing object's keys rather than the schema itself
-  //  * options.skipCast                     -- To know what casts need to be skipped
-  //  * options.skipParams                   -- Won't apply specific params for specific fields
-  //
-  //  * this.structure[ fieldName ].required -- To skip cast if it's `undefined` and it's NOT required
-  //
-  // Note that the only special parameter is 'required' -- it's only special because _cast() won't cast
-  // it if it's `undefined` and it's not required. Otherwise, casting will make validation fail for unrequired and absent values
-  //
-  // This will run _cast and _param
-  validate (originalObject, options) {
-    var self = this
-
-    options = typeof (options) === 'undefined' ? {} : options
-
-    let { castObject, failedCasts, failedRequired } = self._cast(originalObject, options)
-    let { paramObject, errors } = self._params(castObject, originalObject, options, failedCasts, failedRequired)
-
-    Object.keys(failedCasts).forEach(function (fieldName) {
-      errors.push({ field: fieldName, message: 'Error during casting' })
-    })
-    return { resultObject: paramObject, errors }
+  _paramToBeSkipped (fieldName, parameterName, skipParams) {
+    if (typeof (skipParams) !== 'object' || skipParams === null) return false
+    if (Array.isArray(skipParams[fieldName]) && skipParams.indexOf(parameterName) !== -1) return true
+    return false
   }
 
   cleanup (object, parameterName) {
@@ -427,6 +338,6 @@ var s = new SimpleSchema({
 
 })
 
-let { resultObject, errors } = s.validate({ name: 'Tony', age: '15' }, { onlyObjectValues: true })
+let { validatedObject, errors } = s.validate({ name: 'Tony', _surname: 'Mobily1234567890', age: '25' }, { __onlyObjectValues: true })
 
-console.log('RESULT:', resultObject, errors)
+console.log('RESULT:', validatedObject, errors)
